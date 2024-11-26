0e5c58b253014219dc5b566a1a91df1e083317e6
30vam <khodaparast2011@gmail.com>
2024-11-26 21:13:46 +0330

    Updated carousel

--- src/components/CarouselCard.jsx
+++ src/components/CarouselCard.jsx
@@ -4,8 +4,10 @@ import './Carousel.css'
 const CarouselCard = ({title, content}) => {
   return (
     <div className='carousel-card'>
-      <h2>{title}</h2>
-      <p>{content}</p>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <h2 className='text-3xl font-bold'>{title}</h2>
+        <p>{content}</p>
+      </div>
     </div>
   );
   
